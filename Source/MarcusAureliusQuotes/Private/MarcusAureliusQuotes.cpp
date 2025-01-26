8e3e288f1ab7ce2774999e3bd2ba01e1690b66c4
yetigit <baidhir.hidair@gmail.com>
2025-01-26 23:14:22 +0100

    update todo

--- Source/MarcusAureliusQuotes/Private/MarcusAureliusQuotes.cpp
+++ Source/MarcusAureliusQuotes/Private/MarcusAureliusQuotes.cpp
@@ -17,9 +17,13 @@ for the quote display
 * [x] use plugin log instead of LogTemp
 * [x] auto size the window based on its content upon displayquote()
 * [x] better window size for the quote display
-* [] add custom CLOSE button (which will hide the window only)
-* [] Have one widget for the quotation, author name will be appended to quote
-text with "\n\n--" suffix
+* [] have the window appear in the corner of the viewport
+* [] prevent the window from stealing focus on show()
+* [] the window should not be native, it should stay in the engine
+* [] when the user click something or is in the middle of viewport interaction
+* the window remains showing and unfocused (on top of unreal only).
+* - we cannot make it a popup window because a popup cannot be moved
+* - a popup does not have a close button
 */
 
 #include "MarcusAureliusQuotesLog.h"
