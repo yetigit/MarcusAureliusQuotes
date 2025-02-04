e182828b3c036c96f5ff89e1bfcfdad48cfa76ba
yetigit <baidhir.hidair@gmail.com>
2025-02-04 20:53:52 +0100

    load plugin on default loading phase

--- Source/MarcusAureliusQuotes/Private/MarcusAureliusQuotes.cpp
+++ Source/MarcusAureliusQuotes/Private/MarcusAureliusQuotes.cpp
@@ -6,8 +6,8 @@
 /*
  * TODO:
  * [x] a picture of the author should appear with the quote and his name
+ * [x] try loading the plugin on default loading phase
  * [] add brushes for seneca and epictetus
- * [] try loading the plugin on default loading phase
  * [] Attempt to change the plugin name
  */
 
